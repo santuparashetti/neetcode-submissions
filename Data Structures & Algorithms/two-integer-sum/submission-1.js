44d5991e440931f3b528226b836e9a767e1ae827
Santosh Parashetti <santuparashetti@users.noreply.github.com>
2026-05-09 12:33:54 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        for (let i = 0;  i < nums.length - 1; i++) {
+            for (let j = 1; j < nums.length; j++) {
+                if (i != j && nums[i] + nums[j] == target) {
+                    return [i, j];
+                }
+            }   
+        }
+    }
+}
